d77d7aa00eabe54abbec91d3e3a5725262d1db9f
alton15 <alton15@naver.com>
2019-11-21 19:19:46 +0900

    필요없는 주석 삭제

--- Moodlet/app/src/main/java/com/example/moodlet/MainActivity.java
+++ Moodlet/app/src/main/java/com/example/moodlet/MainActivity.java
@@ -33,15 +33,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         calendarView = findViewById(R.id.calendarView);
 
-//        Button btnRange = findViewById(R.id.btnRange);
-//        btnRange.setOnClickListener(this);
-//
-//        Button btnWithoutAnim = findViewById(R.id.btnWithoutAnim);
-//        btnWithoutAnim.setOnClickListener(this);
-//
-//        Button btnWithAnim = findViewById(R.id.btnWithAnim);
-//        btnWithAnim.setOnClickListener(this);
-
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
@@ -58,39 +49,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view) {
 
-        switch (view.getId()) {
-//            case R.id.btnWithAnim:
-//                calendarView.setDate(calendar.getTimeInMillis(), true, true);
-//                break;
-//
-//            case R.id.btnWithoutAnim:
-//                calendar.set(Calendar.DAY_OF_MONTH, 12);
-//                calendar.set(Calendar.YEAR, 2016);
-//                calendar.add(Calendar.MONTH, Calendar.MARCH);
-//                calendarView.setDate(calendar.getTimeInMillis(), false, false);
-//                break;
-//
-//            case R.id.btnRange:
-//
-//                Calendar calendar = Calendar.getInstance();
-//                calendar.set(Calendar.DATE, calendar.getActualMaximum(Calendar.DATE));
-//                long endOfMonth = calendar.getTimeInMillis();
-//                calendar = Calendar.getInstance();
-//                calendar.set(Calendar.DATE, 1);
-//                calendar.set(Calendar.HOUR_OF_DAY, 0);
-//                long startOfMonth = calendar.getTimeInMillis();
-//                calendarView.setMaxDate(endOfMonth);
-//                calendarView.setMinDate(startOfMonth);
-//
-//
-//                String minDateString = new SimpleDateFormat("MM/dd/yyyy").format(new Date(calendarView.getMinDate()));
-//                String maxDateString = new SimpleDateFormat("MM/dd/yyyy").format(new Date(calendarView.getMaxDate()));
-//
-//                Toast.makeText(getApplicationContext(), "MMDDYYYY Min date - " + minDateString + " Max Date is " + maxDateString, Toast.LENGTH_LONG).show();
-//
-//                break;
-
-
-        }
     }
 }
